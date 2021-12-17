efb84fdcb47b189682cebc7074c51fa94c7b3fde
Veronika Malyshkova <malyshkovav@mac-profi.ru>
2021-12-17 18:58:02 +0300

    refactor: create common function

--- slider/app.js
+++ slider/app.js
@@ -1,15 +1,21 @@
-const slides = document.querySelectorAll('.slide');
+function slidesPlugin(activeSlide) {
+  const slides = document.querySelectorAll('.slide');
 
-for (const slide of slides) {
-  slide.addEventListener('click',  () => {
-    clearActiveClasses();
-
-    slide.classList.add('active');
-  });
+  slides[activeSlide].classList.add('active');
+  
+  for (const slide of slides) {
+    slide.addEventListener('click',  () => {
+      clearActiveClasses();
+  
+      slide.classList.add('active');
+    });
+  };
+  
+  const clearActiveClasses = () => {
+    slides.forEach((slide) => {
+      slide.classList.remove('active');
+    });
+  };
 };
 
-const clearActiveClasses = () => {
-  slides.forEach((slide) => {
-    slide.classList.remove('active');
-  });
-};
+slidesPlugin(0);
